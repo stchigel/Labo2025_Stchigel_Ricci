f52d45121c12c9880c5fcc2a3b9f1816d17be859
stchigel <alumno26.stchigel.grimaldi.nicolas@ipm.edu.ar>
2025-03-10 12:43:49 -0300

    Seguimos con el 2

--- C- -/Ejercicios práctica CPP/2.cpp
+++ C- -/Ejercicios práctica CPP/2.cpp
@@ -12,5 +12,37 @@ struct Producto{
 };
 
 int main(){
-    
+    Producto pasillo[3][4];
+    float sump=0; //suma de los precios
+    float promp=0; //promedio
+    float maxp=0; //precio mayor
+    int maxc=0; //columna del p. mayor
+    for (int i = 0; i < 3; i++) { 
+        for (int j = 0; j < 4; j++) { 
+            cout<<"Producto fila "<<i<<" columna "<<j<<endl;
+            cout<<"Ingrese nombre"<<endl;
+            cin>>pasillo[i][j].nombre;
+            cout<<"Ingrese marca"<<endl;
+            cin>>pasillo[i][j].marca;
+            cout<<"Ingrese codigo"<<endl;
+            cin>>pasillo[i][j].codigo;
+            cout<<"Ingrese precio"<<endl;
+            cin>>pasillo[i][j].precio;
+            cout<<"Vence o no? 0=No 1=Si"<<endl;
+            cin>>pasillo[i][j].vencimiento;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) { 
+        for (int j = 0; j < 4; j++) { 
+            Producto prod = pasillo[i][j];
+            if (prod.precio>maxp){
+                maxc=j;
+            }
+        }
+    }
+
+    cout<<"Producto fila 2 columna 3"<<endl;
+    cout<<pasillo[2][3].nombre<<endl;
+
 }
