b75496d4f137eb6fa85e3023c55f7ddcaee2d441
stchigel <alumno26.stchigel.grimaldi.nicolas@ipm.edu.ar>
2025-04-07 09:40:06 -0300

    Ejercicios 9 version 2

--- Labo-Java-2025/src/unid0/PalabrasIguales.java
+++ Labo-Java-2025/src/unid0/PalabrasIguales.java
@@ -10,19 +10,11 @@ public class PalabrasIguales {
         String palabra1=e.nextLine();
         System.out.println("Ingresa la segunda palabra");
         String palabra2=e.nextLine();
-        for(int i=0; i<palabra1.length();i++){
-            if(palabra1.charAt(i)==palabra2.charAt(i)){
-                aux = aux + 1;
-            }
-            else{
-                aux = aux - 1;
-            }
-        }
-        if(aux==palabra2.length()){
+        if(palabra1.equals(palabra2)){
             System.out.println("La 2 palabras son iguales");
         }
         else{
-            System.out.println("Las 2 palabras no son iguales");
+            System.out.println("La 2 palabras no son iguales");
         }
     }
 }
