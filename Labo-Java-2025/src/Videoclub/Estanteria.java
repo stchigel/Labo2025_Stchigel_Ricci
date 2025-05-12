19a727a3d4b7334d562a69ff900f8c042aa72f69
stchigel <alumno26.stchigel.grimaldi.nicolas@ipm.edu.ar>
2025-05-12 12:44:38 -0300

    Empezamos con junit

--- Labo-Java-2025/src/Videoclub/Estanteria.java
+++ Labo-Java-2025/src/Videoclub/Estanteria.java
@@ -1,12 +1,23 @@
 package Videoclub;
 
+import org.junit.jupiter.api.*;
 import unidad1.Persona;
 
 import java.util.ArrayList;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class Estanteria {
     private int numero;
     private ArrayList<Pelicula> peliculas;
+
+    @BeforeEach
+    public void setUp(){
+        numero=2;
+        ArrayList<Pelicula> peliculas=new ArrayList<>();
+    }
 
     public Estanteria(int numero, ArrayList<Pelicula> peliculas) {
         this.numero = numero;
@@ -39,6 +50,22 @@ public class Estanteria {
 
     public void addPelicula(Pelicula pelicula) {
         this.peliculas.add(pelicula);
+    }
+    @Test
+    public void agregarPeli(){
+        ArrayList<Persona> directores1 = new ArrayList<>();
+        directores1.add(new Persona("Robert Zemeckis", 72, "N/A"));
+        ArrayList<Persona> actores1 = new ArrayList<>();
+        actores1.add(new Persona("Michael J. Fox", 63, "N/A"));
+        ArrayList<String> idiomas1 = new ArrayList<>();
+        idiomas1.add("Español");
+        idiomas1.add("Inglés");
+        idiomas1.add("Alemán");
+        idiomas1.add("Árabe");
+        Pelicula p1 = new Pelicula("Volver al Futuro", "Ciencia Ficción", 116, directores1, actores1, idiomas1);
+        assertEquals(0, peliculas.size());
+        addPelicula(p1);
+        assertEquals(1, peliculas.size());
     }
 
     public void removePelicula(Pelicula pelicula) {
